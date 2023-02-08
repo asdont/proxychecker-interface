dc3e5ee368c42f71d7cb8ec2b0e0d100bcd6c2c3
asd192 <PanshinDA@yandex.ru>
2023-02-08 23:02:20 +0500

    Skeleton.

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,16 @@
 </script>
 
 <template>
-  <main>
-    <h1>Proxy Checker Interface</h1>
-  </main>
+  <header>
+    <div id="title">PROXY CHECKER</div>
+  </header>
+    <div id="left">left</div>
+    <div id="center-top">c-top</div>
+    <div id="center-bottom">c-bottom</div>
+    <div id="right">right</div>
+  <footer>(C) 2023</footer>
 </template>
 
 <style scoped>
-@media (min-width: 1024px) {
 
-}
 </style>
